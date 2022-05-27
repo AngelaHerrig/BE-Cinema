72a9a128ac694b03c4948de3a9bd7c89eb89ea2b
Angela Herrig <angela.k.herrig@gmail.com>
2022-05-27 10:47:18 +0200

    update 1

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -7,7 +7,6 @@ export function checkAuth(req, res, next) {
     const token = authHeader.split(" ")[1];
 
     jwt.verify(token, process.env.SECRET, async (error, payload) => {
-      // const user = await User.findById(payload.uid);
       req.userId = payload.uid;
 
       next();
@@ -22,7 +21,6 @@ export function checkAuth(req, res, next) {
 }
 
 export async function checkAdmin(req, res, next) {
-  /////
   try {
     const authHeader = req.headers.authorization;
     const token = authHeader.split(" ")[1];
@@ -40,36 +38,4 @@ export async function checkAdmin(req, res, next) {
       originalError: error,
     });
   }
-
-  ////
-
-  // const token = req.headers.authorization;
-
-  // if (!token) {
-  //   return res.status(403).send("Admins only");
-  // }
-
-  // try {
-  //   const pureTokenArr = token.split(" ");
-  //   const pureToken = pureTokenArr[1];
-
-  //   jwt.verify(pureToken, process.env.SECRET, async (error, payload) => {
-  //     if (error) {
-  //       res.status(400).send({ error: error.message });
-  //       return;
-  //     }
-  //     const user = await User.findById(payload.uid);
-
-  //     if (user.status !== "admin") {
-  //       return res.status(403).send("Admins only");
-  //     }
-  //     next();
-  //   });
-  // } catch (error) {
-  //   next({
-  //     status: 401,
-  //     message: "Access denied",
-  //     originalError: error,
-  //   });
-  // }
 }
